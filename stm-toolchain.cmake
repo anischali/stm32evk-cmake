33cd4cf3e8616716213fbad4900edcc64ffb4d53
Anis CHALI <anis.chali@exfo.com>
2024-03-09 20:28:45 -0500

    cmake: refactor cmake toolchain files

--- stm-toolchain.cmake
+++ stm-toolchain.cmake
@@ -11,43 +11,21 @@ set(CMAKE_SYSTEM_PROCESSOR arm)
 set(CMAKE_CROSSCOMPILING 1)
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
-
-
-# add_compile_options(
-#     -D${STM32_PROJECT_PTYPE} -mtune=cortex-m0 -g -ggdb -Os -g3 -Wall -fmessage-length=0 
-#     -ffunction-sections -fdata-sections -ffreestanding -flto
-#     -pipe -Wall -Wstrict-prototypes -Wmissing-prototypes -Werror-implicit-function-declaration 
-#     -Wpointer-arith -std=gnu99 -fno-strict-aliasing -ffunction-sections -fdata-sections 
-#     -Wchar-subscripts -Wcomment -Wformat=2 -Wimplicit-int -Wmain -Wparentheses -Wsequence-point 
-#     -Wreturn-type -Wswitch -Wtrigraphs -Wunused -Wuninitialized -Wunknown-pragmas -Wfloat-equal 
-#     -Wundef -Wshadow -Wbad-function-cast -Wwrite-strings -Wsign-compare -Waggregate-return 
-#     -Wmissing-declarations -Wformat -Wmissing-format-attribute -Wno-deprecated-declarations 
-#     -Wpacked -Wredundant-decls -Wnested-externs -Wlong-long -Wunreachable-code -Wcast-align 
-#     --param max-inline-insns-single=500
-# )
-
 add_compile_options(
-    -std=gnu11 -mcpu=${STM32_PROJECT_CPU} 
-    -mtune=${STM32_PROJECT_CPU} -g -ggdb -Os -g3 -Wall -fmessage-length=0 
-    -ffunction-sections -fdata-sections -ffreestanding -flto -nostdlib
-    ${STM32_MFLOAT_ABI} -specs=nano.specs -mthumb -Wall -MD
+    -std=gnu11 -mcpu=${STM32_PROJECT_CPU} -nostdlib -specs=nano.specs
+    -ffunction-sections -fdata-sections -ffreestanding -flto -fno-strict-aliasing
+    -mthumb -mtune=${STM32_PROJECT_CPU} -g -ggdb -Og -g3 -Wall -fmessage-length=0 
+    ${STM32_MFLOAT_ABI} -Wall -MD
 )
+
 set (CMAKE_ASM_FLAGS "${CMAKE_CXX_FLAGS} -x assembler-with-cpp")
 
-#add_link_options(
-#    -std=gnu11 -mcpu=${STM32_PROJECT_CPU} -Wl,--entry=Reset_Handler -Wl,--cref -mthumb
-#    -T${CMAKE_CURRENT_BINARY_DIR}/../${STM32_PROJECT_LDSCRIPT} -Wl,--print-memory-usage
-#    -mthumb -flto -ffreestanding -mtune=${STM32_PROJECT_CPU} -nostartfiles -nostdlib
-#    -Xlinker --Map=${STM32_PROJECT_NAME}.map -specs=nosys.specs -specs=nano.specs 
-#    -static -Wl,--start-group -lc -lm -Wl,--end-group
-#)
-
 add_link_options(
-    -std=gnu11 -T${CMAKE_CURRENT_BINARY_DIR}/../${STM32_PROJECT_LDSCRIPT} -Xlinker --gc-sections 
-    -specs=nosys.specs -Wl,-Map=${STM32_PROJECT_NAME}.map -Wl,--gc-sections -static -Wl,--no-warn-rwx-segment
-    -Wl,--start-group -lc -lm -Wl,--end-group -mtune=${STM32_PROJECT_CPU} -Wl,--print-memory-usage
+    -mthumb -specs=nosys.specs -Wl,-Map=${STM32_PROJECT_NAME}.map 
+    -std=gnu11 -Wl,--start-group -lm  -Wl,--end-group -Wl,--gc-sections -g3 -Os -ggdb 
+    -mcpu=${STM32_PROJECT_CPU} -Wl,--entry=Reset_Handler -Wl,--cref -Wl,--print-memory-usage 
+    -T${CMAKE_CURRENT_BINARY_DIR}/../${STM32_PROJECT_LDSCRIPT} 
 )
-
 
 add_link_options(${STM32_LIB_EXTRA_LIBS})
 add_definitions(
